3c7f8ab93e337eda8374106c0eb47499469fd218
duraisamynaveen <35652997+duraisamynaveen@users.noreply.github.com>
2018-03-15 16:24:06 +0530

    Create concatenate.c

---
+++ concatenate.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str1[20],str2[20];
+int comcatenate;
+printf("enter the first string:");
+scanf("%s",&str1);
+printf("enter the second string:");
+scanf("%s",&str2):
+concatenate=str1+str2;
+printf("the string is",concatenate);
+return 0;
+}
